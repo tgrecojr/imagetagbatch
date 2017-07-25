c67a1d2201aa8b46b07af392979439dcfc5552a4
T.J. Greco <tgrecojr@gmail.com>
2017-07-24 21:14:07 -0400

    fixes #2

---
+++ src/main/java/com/greco/imagetag/repo/LabelRepository.java
@@ -0,0 +1,7 @@
+package com.greco.imagetag.repo;
+
+import com.greco.imagetag.model.Label;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LabelRepository extends CrudRepository<Label, Long> {
+}
