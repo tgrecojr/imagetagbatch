e307bb8dc1a38ff9856a11f7c1871a06afa40bbf
T.J. Greco <tgrecojr@gmail.com>
2017-07-25 06:25:37 -0400

    adding unit test for calculation of SHA1 object key name

--- src/main/java/com/greco/imagetag/ImagetaggingApplication.java
+++ src/main/java/com/greco/imagetag/ImagetaggingApplication.java
@@ -1,10 +1,10 @@
 package com.greco.imagetag;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages={"com.greco.imagetag.*"})
